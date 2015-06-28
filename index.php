70daabefc1481ce0c265d65508acb2577f853ea3
ahmedanwer <ahmedmuhanwar@gmail.com>
2015-06-28 04:10:10 +0200

    initializing api post Format

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+class InputFunction
+{
+public  $key;
+public  $FunctionName;
+public  $inputs=array();
+}
+
+$data = json_decode(file_get_contents('php://input'));
+if($data)
+{
+  if(ValidateKEY($data->key))
+  {
+  $data->FunctionName;
+  call_user_func($data->FunctionName,$data->inputs);
+  }
+}
+function ValidateKEY($key)
+{
+if($key>=-1)
+{
+  return true;
+}
+}
+
+//***********Functions Start here //
+
+function Register($a)
+{
+  echo $a[0];
+}
+
+ ?>
