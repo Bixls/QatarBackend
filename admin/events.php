1f8cfbfea5aebb40c7b6285d080658b49d5fc4ff
ahmedanwer <ahmedmuhanwar@gmail.com>
2015-07-14 10:02:30 +0200

    Approving Event

--- admin/events.php
+++ admin/events.php
@@ -54,6 +54,10 @@ public function SearchEventsByName($searcher){
 
 }
 public function Approve($id){
+  global $db;
+  $db->update("Events",array('approved' =>1), $where=array("id"=>$id));
+  include("functions/generalFunctions.php");
+  messege("alert-success","Sucess      ","The Event has been approved successfully");
 
 }
 public function Disapprove($id){
@@ -65,6 +69,25 @@ public function RemoveEvent($id){
 public function ViewEvent($id){
 
 }
+
+
+
+public function DisapproveEventbyID($inputs){
+  require_once("DataBaseConnection.php");
+  $dbConnect=new DatabaseConnect;
+  $sql=mysql_query("SELECT `VIP` FROM `Events` WHERE `Events`.`id`=".$inputs->Eventid) or die (mysql_error());
+  if($sql){
+      $row=mysql_fetch_array($sql);
+  if($row["VIP"]==1){
+    //Give the user back his points
+  }
+  else{
+    // Just Send the dissaproval messege ي
+  }
+  }
+    $dbConnect->close();
+}
+
 
 }
 
