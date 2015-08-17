4a441ea4023dc1406db09e8e245e8b0d7a06dc59
ahmedanwer <ahmedmuhanwar@gmail.com>
2015-08-17 05:53:27 +0200

    sorting groups

--- includes/Group.php
+++ includes/Group.php
@@ -19,7 +19,7 @@ public  function getGroupList($inputs){
 
   require_once("DataBaseConnection.php");
   $dbConnect=new DatabaseConnect;
-  $query = mysql_query("SELECT * FROM `groups` LIMIT ".$inputs->limit."") or die (mysql_error());
+  $query = mysql_query("SELECT * FROM `groups` ORDER BY `groups`.`priority`  LIMIT ".$inputs->limit."  ") or die (mysql_error());
   $stack = array();
       while($row = mysql_fetch_array($query)){
           $respond = array(
